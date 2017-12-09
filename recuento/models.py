960553aec5f46011988ab8267036a4a3afe8c527
ernestodtv <ernestodtv@gmail.com>
2017-12-09 11:52:57 +0100

    feat: añade el dominio en Django
    Añade al archivo models.py los modelos de dominio en Django.
    
    Closes #4

--- recuento/models.py
+++ recuento/models.py
@@ -3,4 +3,41 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    start_date = models.DateTimeField
+    end_date = models.DateTimeField
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=200)
+    optional = models.BooleanField(default=False)
+    multiple = models.BooleanField(default=False)
+    poll = models.ForeignKey(Poll, null=False, on_delete=models.CASCADE)
+
+
+class QuestionOption(models.Model):
+    description = models.TextField(max_length=200)
+    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
+
+
+class OptionPerVote(models.Model):
+    question_option = models.ForeignKey(QuestionOption, on_delete=models.CASCADE)
+    vote = models.ForeignKey(Vote, on_delete=models.CASCADE)
+
+
+VOTE_TYPE = (
+
+        ('0', ' WEB'),
+        ('1', 'SLACK'),
+        ('2', 'TELEGRAM')
+    )
+
+
+class Vote(models.Model):
+    vote_type = models.CharField(max_length=1, choices=VOTE_TYPE)
+    vote_date = models.DateTimeField
+    token = models.CharField(max_length=50)
+    question_options = models.ManyToManyField(QuestionOption, through='OptionPerVote')
