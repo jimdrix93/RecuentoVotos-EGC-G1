7b85cb7c725e5013b3709db4aa9c0dd9166a3a93
Pablo Roldán Sánchez <pabrolsan@alum.us.es>
2017-12-11 17:11:12 +0100

    Añadido esqueleto para realizar los recuentos estilo approve
    Se ha añadido un scritp llamado CountMethods.py en el cual se va a definir los
     distintos tipo de recuento, en este se ha definido el esqueleto a base de comentarios
    del metodo para el recuento estilo approve
    
    affects #10

---
+++ recuento/CountMethods.py
@@ -0,0 +1,13 @@
+
+
+def approvecount(pollid):
+    pass
+    '''
+    Obtener la lista de preguntas de la poll
+    '''
+    '''
+    Por cada pregunta obtener la lista de opciones de la pregunta
+    '''
+    '''
+    Por cada opcion de la pregunta miramos la longitud del set_optionPerVote->Numero de veces que se ha votado esa opcion
+    '''
