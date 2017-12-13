1f2f43ea0759ae2c1def5bb1505642decc869de7
Pablo Roldán Sánchez <pabrolsan@alum.us.es>
2017-12-13 15:31:53 +0100

    Realizado el metodo de recuento por aprobacion
    Se ha realizado el script, este lo que hace es contar los objetos OptionPerVote de cada opcion de una
    respuesta y crea un objeto result para cada opción tal como se habia planeado
    
    affects #10

--- recuento/CountMethods.py
+++ recuento/CountMethods.py
@@ -1,13 +1,19 @@
-
-
+from recuento.models import Poll
+from recuento.models import Result
 def approvecount(pollid):
     pass
     '''
     Obtener la lista de preguntas de la poll
     '''
+    obj = Poll.objects.get(pk=pollid)
     '''
     Por cada pregunta obtener la lista de opciones de la pregunta
     '''
-    '''
-    Por cada opcion de la pregunta miramos la longitud del set_optionPerVote->Numero de veces que se ha votado esa opcion
-    '''
+
+    for question in obj.question_set.all():
+        '''
+        #   Por cada opcion de la pregunta miramos la longitud del set_optionPerVote->Numero de veces que se ha votado esa opcion
+        '''
+        QuestionOption=question.questionoption_set.all()
+        for option in QuestionOption:
+           new_result = Result.object.get_or_create(question_option =option,quantity=len(option.optionpervote_set.all))
